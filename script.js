9069a6a9e8532c2785ada4ce7cb335975dba5bbd
dandy.alv <dandy.alv@gmail.com>
2022-06-21 16:52:51 +0800

    final commit

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 let fetchAll = async () => {
-    const url = "https://feriirawan-api.herokuapp.com/list/symbols/province/200";
+    const url = "https://restcountries.com/v3.1/all";
     const response = await fetch(url, {
         method: "GET"
     });
 
     const json = await response.json();
 
-    allData = json.lambang;
+    // allData = json.lambang;
 
-    console.log(allData);
+    console.log(json);
 
-    return allData;
+    return json;
 }
 
 let displayResult = async () => {
@@ -20,12 +20,13 @@ let displayResult = async () => {
     data.forEach((res) => {
         document.getElementById("cards").insertAdjacentHTML(
             'beforeend',
-            `<div class="card pt-3 pl-1 pr-1 m-3" style="width: 18rem;">
-                <img class="card-img-top" src=${res.url} alt="${res.title}">
+            `<div class="card pt-5 pl-1 pr-1 m-3" style="width: 18rem;">
+                <img class="card-img-top" src=${res.flags.svg} alt="${res.name.common}">
                 <div class="card-body">
-                    <h5 class="card-title">${res.title}</h5>
+                    <h5 class="card-title mt-2 mb-2">${res.name.common}</h5>
                 </div>
             </div>`
         );
     })
+
 }
